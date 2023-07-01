6b9299926e959a2cc659cb37f9d12d6a202efd39
victorgiuliano94@gmail.com <victorgiuliano94@gmail.com>
2023-07-01 12:55:52 +0000

    finalizada dim employees

--- models/staging/erp_northwind/stg_erp_employees.sql
+++ models/staging/erp_northwind/stg_erp_employees.sql
@@ -2,11 +2,10 @@ with
     source_employees as (
         select
             cast(employee_id as int) as funcionario_id
+            , reports_to as gerente_id
             , cast(last_name as string) as sobrenome
             , cast(first_name as string) as nome
             , cast(first_name || ' ' || last_name as string) as nome_completo
-            --, title
-            --, title_of_courtesy
             , cast(birth_date as date) as data_nascimento
             , cast(hire_date as date) as data_contratacao
             , cast(address as string) as endereco
@@ -14,11 +13,7 @@ with
             , cast(region as string) as regiao
             , cast(postal_code as string) as cep
             , cast(country as string) as pais
-            --, home_phone
-            --, extension
             , notes as notas
-            , reports_to as gerente_id
-            --, photo_path
         from {{ source('erp', 'employees') }}
     )
 select *
